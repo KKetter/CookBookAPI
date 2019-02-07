ebd5efd2bec63fd2bba90d445a377781c6521ed6
Kent Ketter <kent.ketter1@gmail.com>
2019-02-06 16:11:19 -0800

    IngredientTests Complete

--- CookBookAPI/XUnitTestAPITests/Ingredients/IngredientsTests.cs
+++ CookBookAPI/XUnitTestAPITests/Ingredients/IngredientsTests.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using CookBookAPI.Controllers;
+﻿using CookBookAPI.Controllers;
 using CookBookAPI.Data;
 using CookBookAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using CookBookAPI;
 using System.Linq;
 
 namespace XUnitTestAPITests
@@ -100,9 +95,9 @@ namespace XUnitTestAPITests
             }
         }
         [Fact]
-        public async void CanEditIngredient()
+        public async void CanEditIngredientToAction()
         {
-            DbContextOptions<CookBookDbContext> options = new DbContextOptionsBuilder<CookBookDbContext>().UseInMemoryDatabase("CanEditIngredient").Options;
+            DbContextOptions<CookBookDbContext> options = new DbContextOptionsBuilder<CookBookDbContext>().UseInMemoryDatabase("CanEditIngredientToAction").Options;
 
             using (CookBookDbContext context = new CookBookDbContext(options))
             {
@@ -122,11 +117,114 @@ namespace XUnitTestAPITests
                 await ingredientsController.Post(ingredient);
                 await ingredientsController.Post(ingredient2);
                 var data = await ingredientsController.Put(3, ingredient3);
-                //var result = context.Ingredients.FirstOrDefault(c => c.ID == ingredient.ID);
 
                 //Assert
                 Assert.IsType<RedirectToActionResult>(data);
             }
         }
+        [Fact]
+        public async void CanEditIngredientBadRequest()
+        {
+            DbContextOptions<CookBookDbContext> options = new DbContextOptionsBuilder<CookBookDbContext>().UseInMemoryDatabase("CanEditIngredientBadRequest").Options;
+
+            using (CookBookDbContext context = new CookBookDbContext(options))
+            {
+                //Arrange
+                Ingredients ingredient = new Ingredients();
+                ingredient.ID = 1;
+                ingredient.Name = "Swag";
+                Ingredients ingredient2 = new Ingredients();
+                ingredient2.ID = 2;
+                ingredient2.Name = "Energy";
+                Ingredients ingredient3 = new Ingredients();
+                ingredient3.ID = 3;
+                ingredient3.Name = "Heart";
+
+                //Act
+                IngredientsController ingredientsController = new IngredientsController(context, configuration);
+                await ingredientsController.Post(ingredient);
+                await ingredientsController.Post(ingredient2);
+                var data = await ingredientsController.Put(6, ingredient3);
+                
+                //Assert
+                Assert.IsType<BadRequestObjectResult>(data);
+            }
+        }
+        [Fact]
+        public async void CanEditIngredientOk()
+        {
+            DbContextOptions<CookBookDbContext> options = new DbContextOptionsBuilder<CookBookDbContext>().UseInMemoryDatabase("CanEditIngredientOk").Options;
+
+            using (CookBookDbContext context = new CookBookDbContext(options))
+            {
+                //Arrange
+                Ingredients ingredient = new Ingredients();
+                ingredient.ID = 1;
+                ingredient.Name = "Swag";
+                Ingredients ingredient2 = new Ingredients();
+                ingredient2.ID = 2;
+                ingredient2.Name = "Energy";
+
+                //Act
+                IngredientsController ingredientsController = new IngredientsController(context, configuration);
+                await ingredientsController.Post(ingredient);
+                await ingredientsController.Post(ingredient2);
+                var data = await ingredientsController.Put(2, ingredient2);
+
+                //Assert
+                Assert.IsType<OkObjectResult>(data);
+            }
+        }
+        [Fact]
+        public async void CanGetOneIngredientNotFound()
+        {
+            DbContextOptions<CookBookDbContext> options = new DbContextOptionsBuilder<CookBookDbContext>().UseInMemoryDatabase("CanGetOneIngredientNotFound").Options;
+
+            using (CookBookDbContext context = new CookBookDbContext(options))
+            {
+                //Arrange
+                Ingredients ingredient = new Ingredients();
+                ingredient.ID = 1;
+                ingredient.Name = "Swag";
+                Ingredients ingredient2 = new Ingredients();
+                ingredient2.ID = 2;
+                ingredient2.Name = "Energy";
+
+                //Act
+                IngredientsController ingredientsController = new IngredientsController(context, configuration);
+                await ingredientsController.Post(ingredient);
+                await ingredientsController.Post(ingredient2);
+                var data = ingredientsController.Get(6);
+
+                //Assert
+                Assert.IsType<NotFoundResult>(data);
+            }
+        }
+        [Fact]
+        public async void CanGetOneIngredientOkResult()
+        {
+            DbContextOptions<CookBookDbContext> options = new DbContextOptionsBuilder<CookBookDbContext>().UseInMemoryDatabase("CanGetOneIngredientOkResult").Options;
+
+            using (CookBookDbContext context = new CookBookDbContext(options))
+            {
+                //Arrange
+                Ingredients ingredient = new Ingredients();
+                ingredient.ID = 1;
+                ingredient.Name = "Swag";
+                Ingredients ingredient2 = new Ingredients();
+                ingredient2.ID = 2;
+                ingredient2.Name = "Energy";
+
+                //Act
+                IngredientsController ingredientsController = new IngredientsController(context, configuration);
+                await ingredientsController.Post(ingredient);
+                await ingredientsController.Post(ingredient2);
+                var data = ingredientsController.Get(2);
+
+                //Assert
+                Assert.IsType<OkObjectResult>(data);
+            }
+        }
+
     }
 }
